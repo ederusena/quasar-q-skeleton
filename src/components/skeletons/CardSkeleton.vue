fef6ccb7d57588e6964d7d2633a5c06e8721d54f
Eder Sena <eder.sena@live.com>
2021-11-28 23:03:35 -0300

    skeleton table and card

---
+++ src/components/skeletons/CardSkeleton.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="row q-ma-sm q-col-gutter-sm">
+      <div class="col-4" v-for="c in cards"
+          :key="c">
+        <q-card style="height: 534px">
+          <q-skeleton height="200px" square />
+          <q-item v-for="l in linhas" :key="l">
+            <q-item-section>
+              <q-item-label>
+                <q-skeleton type="text" />
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+        </q-card>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TableSkeleton',
+  props: {
+    cards: {
+      type: Number,
+      default: 3,
+      required: false
+    },
+    linhas: {
+      type: Number,
+      default: 3,
+      required: false
+    }
+  }
+}
+</script>
---
+++ src/pages/Card.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-page>
+    <div class="row q-ma-sm q-col-gutter-sm" v-if="load">
+        <div class="col-4" v-for="c in 6"
+          :key="c">
+          <q-card>
+            <img src="https://cdn.quasar.dev/img/mountains.jpg">
+            <q-card-section>
+              <div class="text-h6">Our Changing Planet</div>
+              <div class="text-subtitle2">by John Doe</div>
+            </q-card-section>
+            <q-card-section class="q-pt-none">
+              {{ lorem }}
+            </q-card-section>
+          </q-card>
+        </div>
+    </div>
+
+    <card-skeleton
+      v-else
+      :cards="6"
+      :linhas="5"
+    />
+
+  </q-page>
+</template>
+
+<script>
+import CardSkeleton from 'components/skeletons/CardSkeleton.vue'
+
+export default {
+  name: 'Card',
+  components: { CardSkeleton },
+  data () {
+    return {
+      load: false,
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.load = true
+    }, 5000)
+  }
+}
+</script>
